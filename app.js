6e35a516288e20b39a4fdd4373e9dc178c6936da
Brent Curl <brentjcurl@gmail.com>
2021-01-31 22:14:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let instagram = document.getElementById('insta');
+let youtube = document.getElementById('youtube');
+
+instagram.onclick = function() {
+    window.open().location.href = 'https://www.instagram.com/currentbmx/';
+};
+
+youtube.onclick = function() {
+    alert("Coming Soon!");
+};
